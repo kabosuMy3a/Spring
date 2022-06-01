023ada253a5a38ad0015afe0da59d62ab01e9a2d
kabosumy3a <my3a3380@gmail.com>
2022-06-01 16:51:53 +0900

    1

---
+++ inflearn/eager-endeavor-love/src/main/java/io/github/kabosumy3a/eagerendeavorlove/EagerEndeavorLoveApplication.java
@@ -0,0 +1,13 @@
+package io.github.kabosumy3a.eagerendeavorlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagerEndeavorLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagerEndeavorLoveApplication.class, args);
+	}
+
+}
