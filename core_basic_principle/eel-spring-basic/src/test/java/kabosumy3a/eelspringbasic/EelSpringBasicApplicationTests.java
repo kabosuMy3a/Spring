ce4dfbc7f1805fe58d58b9872bd5ab2f4c9f63b3
kabosumy3a <my3a3380@gmail.com>
2022-07-05 22:53:12 +0900

    c1

---
+++ core_basic_principle/eel-spring-basic/src/test/java/kabosumy3a/eelspringbasic/EelSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package kabosumy3a.eelspringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EelSpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
